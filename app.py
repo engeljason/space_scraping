ebc9f2cc70aa7caf97ef8300b892e60db8a3af78
Jason Engel <engel.jason@gmail.com>
2021-08-14 19:38:29 -0700

    initial html skeleton

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
+import scraper
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
 
 
 #########################
 ## Flask App
 #########################
+app = Flask(__name__)
 
+
+@app.route("/")
+def page():
+    return ""
+
+
+if __name__ == '__main__':
+    app.run()
